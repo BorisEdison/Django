54396f051bc9080e5175c93f1c3e4162d0b43415
Boris <borisedison@gmail.com>
2022-02-20 12:44:20 +0530

    Django templates

--- src/trydjango/trydjango/urls.py
+++ src/trydjango/trydjango/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view,contact_view           # from pages import views
+from pages.views import home_view,contact_view,about_view           # from pages import views
 
 urlpatterns = [
     path('',home_view,name='home'),           # path('',views.home_view,name='home')
+    path('about/',about_view),
     path('contact/',contact_view), 
     path('admin/', admin.site.urls),            
 ]
